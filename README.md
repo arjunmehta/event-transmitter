cd27f905a2aaa443122d5e09a7391b1855e2ea15
Arjun Mehta <arjunmeht@gmail.com>
2015-02-21 15:59:42 -0400

    Adding all base files based on stream-headerfooter

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# stream-event-transmitter
-Transmit (and emit) events over streams.
+# EventTransmitter for Streams
+Attach events to any stream channel and emit those events at a receiving stream endpoint.
+
+EventTransmitter transmits events and their parameters along standard stream pipelines. It requires two instances: one to transmit encoded event information, and another to listen for and emit events, sanitizing the stream of event metadata in the process.
+
+### Install
+```bash
+npm install --save event-transmitter
+```
+
+
+### Include
+```javascript
+var EventTransmitter = require('event-transmitter')
+var et = new EventTransmitter()
+```
+
+### Transmit Events
+Use EventTransmitter to transmit events (works like `ee.emit`). But first you must pipe your EventTransmitter instance to a stream that is connected to where you ultimately want to receive and handle events.
+
+```javascript
+et.pipe(outgoing_stream) // pipe EventTransmitter to some outgoing stream
+et.transmit('header', {something: 'else', oo: [aaaa]}) // transmit an event through that outgoing stream
+```
+
+### Receive and Emit Events Locally
+Now you can listen downstream for EventTransmitter events.
+
+```javascript
+incoming_stream.pipe(et.listen()).pipe(process.stdout)
+et.on('header', function(obj){
+    console.log('emitting a "header" event with object:', obj)
+})
+```
+
+
+### Important
+**Note:** *EventTransmitter should be placed in your pipeline where no data is otherwise read. Your EventTransmitter instance will sanitize stream content as it passes through its listener, removing event metadata from the stream.*
+
+Have a look at the few examples in the package to see how to transmit events between various types of streams, sockets and even processes.
+
---
+++ test/test.js
@@ -0,0 +1,297 @@
+var stream = require('stream');
+var PassThrough = stream.PassThrough || require('readable-stream').PassThrough;
+
+var headerfooter = require('../main');
+var brake = require('brake');
+
+exports['Exported Properly'] = function(test) {
+    test.expect(3);
+
+    test.equal(typeof headerfooter, 'object');
+    test.equal(typeof headerfooter.In, 'function');
+    test.equal(typeof headerfooter.Out, 'function');
+
+    test.done();
+};
+
+exports['New Out'] = function(test) {
+    test.expect(2);
+
+    var add_headerfooter = new headerfooter.Out();
+    add_headerfooter.header = {
+        name: "streamA",
+        metadata: [23, 33, 221, 222]
+    };
+    add_headerfooter.footer = {
+        exit_code: 2
+    };
+
+    test.equal(typeof add_headerfooter.header, 'object');
+    test.equal(typeof add_headerfooter.footer, 'object');
+
+    test.done();
+};
+
+exports['New In'] = function(test) {
+
+    test.expect(1);
+
+    var read_headerfooter = new headerfooter.In();
+
+    test.equal(typeof read_headerfooter, 'object');
+
+    test.done();
+};
+
+exports['Test Parse'] = function(test) {
+    test.expect(6);
+
+    var passthroughA = new PassThrough(); // dummy
+    var passthroughB = new PassThrough(); // dummy
+
+    var add_headerfooter = new headerfooter.Out();
+
+    add_headerfooter.header = {
+        name: "streamA",
+        metadata: [23, 33, 221, 222]
+    };
+    add_headerfooter.footer = {
+        exit_code: 2
+    };
+
+    var read_headerfooter = new headerfooter.In();
+
+    read_headerfooter.on('header', function(header) {
+        test.equal(typeof header, 'object');
+        test.equal(header.name, 'streamA');
+        test.equal(header.metadata[0], 23);
+        test.equal(header.metadata[3], 222);
+    });
+
+    read_headerfooter.on('footer', function(footer) {
+        test.equal(typeof footer, 'object');
+        test.equal(footer.exit_code, 2);
+        test.done();
+    });
+
+    read_headerfooter.on('data', function(data) {
+        console.log('Stream Data:', data.toString()); // sanitized stream data without header and footer data in buffer
+    });
+
+    passthroughA.pipe(add_headerfooter).pipe(passthroughB);
+    passthroughB.pipe(read_headerfooter);
+
+    passthroughA.write('Testing... 123');
+    passthroughA.end("...Ending");
+};
+
+
+exports['No Header or Footer'] = function(test) {
+    test.expect(1);
+
+    var passthroughA = new PassThrough(); // dummy
+    var passthroughB = new PassThrough(); // dummy
+
+    var add_headerfooter = new headerfooter.Out();
+    var read_headerfooter = new headerfooter.In();
+
+    read_headerfooter.on('header', function(header) {
+        console.log("HEADER", header);
+        test.equal(false, true);
+    });
+
+    read_headerfooter.on('footer', function(footer) {
+        console.log("Footer", footer);
+        test.equal(false, true);
+    });
+
+    read_headerfooter.on('data', function(data) {
+        console.log('Stream Data:', data.toString()); // sanitized stream data without header and footer data in buffer
+    });
+
+    read_headerfooter.on('end', function() {
+        test.equal(true, true);
+        test.done();
+    });
+
+    passthroughA.pipe(add_headerfooter).pipe(passthroughB);
+    passthroughB.pipe(read_headerfooter);
+
+    passthroughA.write('Testing... 123');
+    passthroughA.end("...Ending");
+};
+
+exports['No Footer'] = function(test) {
+    test.expect(5);
+
+    var passthroughA = new PassThrough(); // dummy
+    var passthroughB = new PassThrough(); // dummy
+
+    var add_headerfooter = new headerfooter.Out();
+    var read_headerfooter = new headerfooter.In();
+
+    add_headerfooter.header = {
+        name: "streamA",
+        metadata: [23, 33, 221, 222]
+    };
+
+    read_headerfooter.on('header', function(header) {
+        test.equal(typeof header, 'object');
+        test.equal(header.name, 'streamA');
+        test.equal(header.metadata[0], 23);
+        test.equal(header.metadata[3], 222);
+    });
+
+    read_headerfooter.on('footer', function(footer) {
+        console.log("Footer", footer);
+        test.equal(false, true);
+    });
+
+    read_headerfooter.on('data', function(data) {
+        console.log('Stream Data:', data.toString()); // sanitized stream data without header and footer data in buffer
+    });
+
+    read_headerfooter.on('end', function() {
+        test.equal(true, true);
+        test.done();
+    });
+
+    passthroughA.pipe(add_headerfooter).pipe(passthroughB);
+    passthroughB.pipe(read_headerfooter);
+
+    passthroughA.write('Testing... 123');
+    passthroughA.end("...Ending");
+};
+
+exports['No Header'] = function(test) {
+    test.expect(3);
+
+    var passthroughA = new PassThrough(); // dummy
+    var passthroughB = new PassThrough(); // dummy
+
+    var add_headerfooter = new headerfooter.Out();
+
+    add_headerfooter.footer = {
+        exit_code: 2
+    };
+
+    var read_headerfooter = new headerfooter.In();
+
+    read_headerfooter.on('header', function(header) {        
+        test.equal(false, true);
+    });
+
+    read_headerfooter.on('footer', function(footer) {
+        test.equal(typeof footer, 'object');
+        test.equal(footer.exit_code, 2);        
+    });
+
+    read_headerfooter.on('end', function() {
+        test.equal(true, true);
+        test.done();
+    });
+
+    read_headerfooter.on('data', function(data) {
+        console.log('Stream Data:', data.toString()); // sanitized stream data without header and footer data in buffer
+    });
+
+    passthroughA.pipe(add_headerfooter).pipe(passthroughB);
+    passthroughB.pipe(read_headerfooter);
+
+    passthroughA.write('Testing... 123');
+    passthroughA.end("...Ending");
+};
+
+
+exports['Test Braked'] = function(test) {
+    test.expect(6);
+
+    var passthroughA = new PassThrough(); // dummy
+    var passthroughB = new PassThrough(); // dummy
+
+    var add_headerfooter = new headerfooter.Out();
+
+    add_headerfooter.header = {
+        name: "streamA",
+        metadata: [23, 33, 221, 222]
+    };
+    add_headerfooter.footer = {
+        exit_code: 2
+    };
+
+    var read_headerfooter = new headerfooter.In();
+
+    read_headerfooter.on('header', function(header) {
+    	console.log("Header", header);
+        test.equal(typeof header, 'object');
+        test.equal(header.name, 'streamA');
+        test.equal(header.metadata[0], 23);
+        test.equal(header.metadata[3], 222);
+    });
+
+    read_headerfooter.on('footer', function(footer) {
+    	console.log("Footer", footer);
+        test.equal(typeof footer, 'object');
+        test.equal(footer.exit_code, 2);
+        test.done();
+    });
+
+    read_headerfooter.on('data', function(data) {
+        console.log('Stream Data:', data.toString()); // sanitized stream data without header and footer data in buffer
+    });
+
+    passthroughA.pipe(add_headerfooter).pipe(passthroughB);
+    passthroughB.pipe(brake(15)).pipe(read_headerfooter);
+
+    passthroughA.write('Testing... 123');
+    passthroughA.end("...Ending");
+};
+
+
+exports['Test Faster Braked'] = function(test) {
+    test.expect(6);
+
+    var passthroughA = new PassThrough(); // dummy
+    var passthroughB = new PassThrough(); // dummy
+
+    var add_headerfooter = new headerfooter.Out();
+
+    add_headerfooter.header = {
+        name: "streamA",
+        metadata: [23, 33, 221, 222]
+    };
+    add_headerfooter.footer = {
+        exit_code: 2
+    };
+
+    var read_headerfooter = new headerfooter.In();
+
+    read_headerfooter.on('header', function(header) {
+    	console.log("Header", header);
+        test.equal(typeof header, 'object');
+        test.equal(header.name, 'streamA');
+        test.equal(header.metadata[0], 23);
+        test.equal(header.metadata[3], 222);
+    });
+
+    read_headerfooter.on('footer', function(footer) {
+    	console.log("Footer", footer);
+        test.equal(typeof footer, 'object');
+        test.equal(footer.exit_code, 2);
+        test.done();
+    });
+
+    read_headerfooter.on('data', function(data) {
+        console.log('Stream Data:', data.toString()); // sanitized stream data without header and footer data in buffer
+    });
+
+    passthroughA.pipe(add_headerfooter).pipe(passthroughB);
+    passthroughB.pipe(brake(5000)).pipe(read_headerfooter);
+
+    passthroughA.write('Testing... 123');
+    passthroughA.end("...Ending");
+};
+
+exports['tearDown'] = function(done) {
+    done();
+};
