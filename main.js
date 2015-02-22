79e92bc25ccd2de06797b0dd98510cedbce71629
Arjun Mehta <arjunmeht@gmail.com>
2015-02-21 20:35:17 -0400

    Specing main EventTransmitter Object

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
-module.exports = exports = {
-    In: require('./lib/In'),
-    Out: require('./lib/Out')
+var util = require('util');
+var stream = require('stream');
+var crypto = require('crypto');
+
+var Listener = require('./lib/Listener');
+var PassThrough = stream.PassThrough || require('readable-stream').PassThrough;
+
+util.inherits(EventTransmitter, PassThrough);
+
+
+// main constructor
+
+function EventTransmitter(opts) {
+
+    PassThrough.call(this);
+
+    opts = opts || {};    
+
+    var hash = crypto.createHash('sha1', opts.delimeter || 'jambalaya');
+    hash.setEncoding('hex');
+    hash.end();
+
+    this.delimiter = hash.read();
+    this.opts = opts;
+}
+
+
+// prototype methods
+
+EventTransmitter.prototype.transmit = function() {
+    var args = Array.prototype.slice.call(arguments);
+    this.push(this.delimiter + JSON.stringify(args) + this.delimiter);
 };
+
+EventTransmitter.prototype.listen = function(stream) {
+    return new Listener(stream, this.opts);
+};
+
+module.exports = exports = EventTransmitter;
